905159cd36601ef3b56522cde8083d22796b7e12
Yuki Mitsui <silphire@gmail.com>
2011-04-03 19:21:40 +0900

    explicit printFileLines function declaration

--- catn.hs
+++ catn.hs
@@ -1,18 +1,19 @@
 import System (getArgs)
 import IO
 
-makeLine :: Int -> String -> String
-makeLine n str = (show n) ++ " " ++ str
-
 catn fileName = bracket (openFile fileName ReadMode)
 			hClose
 			(\h -> do hSetBuffering h LineBuffering
 				  printFileLine 1 h)
-  where printFileLine n hh = do eof <- hIsEOF hh
-				if eof then return ()
-				       else do str <- hGetLine hh
-					       putStrLn $ makeLine n str
-					       printFileLine (n + 1) hh
+  where 
+    printFileLine :: Int -> Handle -> IO ()
+    printFileLine n hh = do eof <- hIsEOF hh
+			    if eof then return ()
+			           else do str <- hGetLine hh
+					   putStrLn $ makeLine n str
+					   printFileLine (n + 1) hh
+    makeLine :: Int -> String -> String
+    makeLine n str = (show n) ++ " " ++ str
 
 main = do args <- getArgs
 	  if (length args) == 0 then putStrLn "file not specified"
