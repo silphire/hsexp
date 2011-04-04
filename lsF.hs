a9bb2bd201e8c995c7ad916736f1a95f0314c357
Yuki Mitsui <silphire@gmail.com>
2011-04-04 23:24:54 +0900

    use case instead of a lot of ifs

--- lsF.hs
+++ lsF.hs
@@ -3,16 +3,16 @@
 --   add suffix "/" if file is a directory
 --   add suffix "@" if file is a symbolic link
 
+import System
 import Directory
 import System.Posix.Files
-import Monad
 
 lsF :: String -> IO ()
 lsF path = getDirectoryContents path >>= printFileName
   where
     printFileName :: [String] -> IO ()
     printFileName []      = return ()
-    printFileName (f:fs)  = do str <- decorate f
+    printFileName (f:fs)  = do str <- decorate $ relPath f
 			       putStrLn str
 			       printFileName fs
     
@@ -20,13 +20,20 @@ lsF path = getDirectoryContents path >>= printFileName
     decorate fileName = 
       do p <- getPermissions fileName
 	 st <- getSymbolicLinkStatus fileName
-	 if (isSymbolicLink st)
-	    then return (fileName ++ "@")
-	    else if (isDirectory st)
-		    then return (fileName ++ "/")
-		    else if (executable p)
-			    then return (fileName ++ "*")
-			    else return fileName
+	 case (p, st) of
+	   (p, st)  | (isSymbolicLink st) -> return (fileName ++ "@")
+		    | (isDirectory st)	  -> return (fileName ++ "/")
+		    | (executable p)	  -> return (fileName ++ "*")
+		    | otherwise		  -> return fileName
 
-main = lsF "."
+    relPath :: String -> String
+    relPath f
+      | path == "."	   = f
+      | (last path) == '/' = path ++ f
+      | otherwise	   = path ++ "/" ++ f
+
+main = do args <- getArgs
+	  case args of
+	    []	  -> lsF "."
+	    args  -> lsF $ head args
 
